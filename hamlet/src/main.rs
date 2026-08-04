22303540fc3ebf43f8505dc809747eb982f014b3
Parth sharma <dankparth@gmail.com>
2026-08-05 04:18:30 +0530

    Added resume

---
+++ hamlet/src/main.rs
@@ -0,0 +1,68 @@
+use aya::{
+    programs::{PerfEvent, perf_event},
+    util::online_cpus,
+};
+#[rustfmt::skip]
+use log::{debug, warn};
+use tokio::signal;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {ret}");
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/hamlet"
+    )))?;
+    match aya_log::EbpfLogger::init(&mut ebpf) {
+        Err(e) => {
+            // This can happen if you remove all log statements from your eBPF program.
+            warn!("failed to initialize eBPF logger: {e}");
+        }
+        Ok(logger) => {
+            let mut logger =
+                tokio::io::unix::AsyncFd::with_interest(logger, tokio::io::Interest::READABLE)?;
+            tokio::task::spawn(async move {
+                loop {
+                    let mut guard = logger.readable_mut().await.unwrap();
+                    guard.get_inner_mut().flush();
+                    guard.clear_ready();
+                }
+            });
+        }
+    }
+    // This will raise scheduled events on each CPU at 1 HZ, triggered by the kernel based
+    // on clock ticks.
+    let program: &mut PerfEvent = ebpf.program_mut("hamlet").unwrap().try_into()?;
+    program.load()?;
+    for cpu in online_cpus().map_err(|(_, error)| error)? {
+        program.attach(
+            perf_event::PerfEventConfig::Software(perf_event::SoftwareEvent::CpuClock),
+            perf_event::PerfEventScope::AllProcessesOneCpu { cpu },
+            perf_event::SamplePolicy::Frequency(1),
+            true,
+        )?;
+    }
+
+    let ctrl_c = signal::ctrl_c();
+    println!("Waiting for Ctrl-C...");
+    ctrl_c.await?;
+    println!("Exiting...");
+
+    Ok(())
+}
